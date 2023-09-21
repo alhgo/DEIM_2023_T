b7be0b9c3c31da8bd3b2249bbfa05325b4e85aee
Alvaro H <correo@laultimapregunta.com>
2023-09-21 17:49:48 +0200

    Instanciar nave mediante bucles

---
+++ Prefabs/Assets/Script/Instanciador.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instanciador : MonoBehaviour
+{
+
+    [SerializeField] int amount = 10;
+    
+    Vector3 initPos = new Vector3(0f, 0f, 4f);
+    Vector3 despl = new Vector3(2f, 2f, 2f);
+
+    [SerializeField] GameObject nave;
+    [SerializeField] Transform padre;
+    
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        for(int i = 0; i < amount; i++)
+        {
+            Instantiate(nave, initPos, Quaternion.identity);
+            initPos = initPos + despl;
+            print(initPos);
+        }
+        
+    }
+
+
+}
