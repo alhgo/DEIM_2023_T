270a71f906782590d52b42010b63ff2c3c27c3fb
Alvaro H <correo@laultimapregunta.com>
2023-09-25 17:43:50 +0200

    Instanciar array aleatorio

--- PruebasTarde/Assets/Scripts/Arrays.cs
+++ PruebasTarde/Assets/Scripts/Arrays.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Arrays : MonoBehaviour
 {
     [SerializeField] string[] saludos;
+    [SerializeField] GameObject[] objects;
 
     // Start is called before the first frame update
     void Start()
     {
+        /*
         //print(saludos.Length);
         //Bucle por los elementos del array
         for(int n = 0; n < saludos.Length; n++)
@@ -21,6 +23,11 @@ public class Arrays : MonoBehaviour
         print("Al azar: " + saludos[i]);
 
         //print(saludos[]);
+        */
+
+        int i = Random.Range(0, objects.Length);
+        //Elemento al azar
+        Instantiate(objects[i]);
     }
 
     // Update is called once per frame
