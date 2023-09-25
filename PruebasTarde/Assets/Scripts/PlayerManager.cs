f2ca40c7e378c385605ae22873f942bb7588e10d
Alvaro H <correo@laultimapregunta.com>
2023-09-25 17:15:26 +0200

    Rotación suavizada con SmoothDamp

--- PruebasTarde/Assets/Scripts/PlayerManager.cs
+++ PruebasTarde/Assets/Scripts/PlayerManager.cs
@@ -9,6 +9,10 @@ public class PlayerManager : MonoBehaviour
     float moveY;
     float moveX;
 
+    //Valores para el suavizado en la rotacion
+    [SerializeField] float smoothTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
+    Vector3 currentRot;
 
     // Start is called before the first frame update
     void Start()
@@ -26,13 +30,17 @@ public class PlayerManager : MonoBehaviour
         //Desplazar un GameObject
         Vector3 despl = Vector3.up * Time.deltaTime * speed * moveY;
         transform.Translate(despl,Space.World);
+        //transform.position += Vector3.forward * Time.deltaTime * speed * moveY;
+        transform.position += Vector3.right * Time.deltaTime * speed * moveX;
         // transform.Translate(Vector3.right * Time.deltaTime * speed * moveX);
 
         //transform.Rotate(Vector3.forward * Time.deltaTime * -speedRotation * moveX);
 
-        transform.rotation = Quaternion.Euler(Vector3.forward * -60f * moveX);
+        //transform.rotation = Quaternion.Euler(Vector3.forward * -60f * moveX);
+        Vector3 vectorRot = Vector3.forward * -60f * moveX;
+        currentRot = Vector3.SmoothDamp(currentRot, vectorRot, ref velocity, smoothTime);
+        transform.eulerAngles = currentRot;
 
-        //transform.position += Vector3.forward * Time.deltaTime * speed * moveY;
-        transform.position += Vector3.right * Time.deltaTime * speed * moveX;
+        
     }
 }
